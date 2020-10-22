e106c31e01fd3ffde7311bb527edc519ac4302a0
Maged Mohammed <maged.mo.it@gmail.com>
2020-10-23 01:39:52 +0200

    first commit

---
+++ Quran/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quran
+//
+//  Created by Maged Omar on 10/23/20.
+//  Copyright © 2020 Maged Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
